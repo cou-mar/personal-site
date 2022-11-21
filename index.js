d123ab8576a02f38ef4c6d1fc7378b359a50abe1
Courtney Marasigan <coumardev@gmail.com>
2022-11-21 16:45:10 -0500

    nav welcome photo socials

---
+++ index.js
@@ -0,0 +1,16 @@
+// const options = {
+//     bottom: '32px', // default: '32px'
+//     right: '32px', // default: '32px'
+//     left: 'unset', // default: 'unset'
+//     time: '0.3s', // default: '0.3s'
+//     mixColor: '#fff', // default: '#fff'
+//     backgroundColor: 'white',  // default: '#fff'
+//     buttonColorDark: 'white',  // default: '#100f2c'
+//     buttonColorLight: '', // default: '#fff'
+//     saveInCookies: true, // default: true,
+//     label: '🌙', // default: ''
+//     autoMatchOsTheme: true // default: true
+//   }
+  
+//   const darkmode = new Darkmode(options);
+//   darkmode.showWidget();
